4bc8d3c7cad0226cbc6fcb12bb14ab0f37c46255
Ward Yorgason <wardyorgason@gmail.com>
2021-03-17 21:09:02 -0600

    restructure for multiple views, added a navbar

---
+++ angular-js/controllers/homeController.js
@@ -0,0 +1,8 @@
+function homeController($scope, $route, $routeParams, $location){
+    $scope.selectedTabId = 0;
+    this.$route = $route;
+    this.$location = $location;
+    this.$routeParams = $routeParams;
+};
+
+
