f1a5c66766dec878fd782df50dd38aa422e67de2
Ward Yorgason <wardyorgason@gmail.com>
2021-03-21 00:59:21 -0600

    ok, sorry this has a bunch of tweaks and changes. I added a "remove last item" button, tweaked a lot of the expreission options and added an "end of line" option.

--- angular-js/controllers/regexHelper.controller.js
+++ angular-js/controllers/regexHelper.controller.js
@@ -18,7 +18,7 @@ function regexHelperController($scope){
             Name: "Date",
             Description: "Date like 01/01/2021 or 5/6/28. Allows a period at the end because of quickbooks",
             Regex: "(\\d{1,2}\\/\\d{1,2}\\/(?:\\d{4}|\\d{2})\.?)",
-            Options: []
+            Options: {}
         },
         {
             Id: 2,
@@ -26,30 +26,53 @@ function regexHelperController($scope){
             Description: `Word like 'dude' or 'Receipt' or 'out-of-stock',
             but also includes words with weird casing like 'McCormick'`,
             Regex: "([A-Za-z\\-]+)",
-            Options: []
+            Options: {}
         },
         {
             Id: 3,
-            Name: "Alphanumeric 'Word' with possible underscores",
-            Description: "Word like 'dude_47' or 'asdf1234'",
-            Regex: "(\\w+)",
-            Options: []
+            Name: "Optional Word",
+            Description: `Just like 'Normal Word', but is optional for cases',
+            where the information isn't standardized'`,
+            Regex: "([A-Za-z\\-]+)?",
+            Options: {
+                Optional: true
+            }
         },
         {
             Id: 4,
-            Name: "Number w/o Currency",
+            Name: "Number w/ Optional Currency",
             Description: `This is for numbers like '$47.52', '50.',
-            '-100.22' or '6,288.99'`,
+            '-100.22' or '6,288.99'. For quickbooks #10023 is supported',
+            'and it will exclude the #. It also allows a period at the end',
+            'that will not get captured.`,
             Regex: "([\\$]?[\\-]?[\\d,]+[\\.]?[\\d]{0,2})",
-            Options: []
+            Options: {}
         },
         {
             Id: 5,
+            Name: "Invoice Number",
+            Description: `This is for integers (no decimal) like 10082',
+            'or varying formats of invoice numbers like '#19293.'',
+            'In the case of the previous, the '#' and '.' will not be captured`,
+            Regex: "\\#?([0-9]+)\\.?",
+            Options: {}
+        },
+        {
+            Id: 6,
             Name: "Anything",
             Description: `This will capture anything (no pattern). It's
             really dependent on accurately tracking the values after it.`,
             Regex: "(.*?)",
-            Options: []
+            Options: {}
+        },
+        {
+            Id: 6,
+            Name: "End of Line",
+            Description: `Matches the end of the line with an optional space before and after`,
+            Regex: "$",
+            Options: {
+                EndOfLine: true
+            }
         }
     ];
 
@@ -57,14 +80,32 @@ function regexHelperController($scope){
 
     $scope.generateRegex = function() {
         var delimiter = "\\s+";
+        var optionalDelimiter = "(?:\\s+)?";
         var finalString = "";
-        for(var control of $scope.currentRegexList){
-            finalString += control.Regex;
-            finalString += delimiter;
+        var finalDelimiter = ""; // this is so we know the last delimiter added
+
+        let listLength = $scope.currentRegexList.length;
+        let listShortName = $scope.currentRegexList;
+        for(let i = 0; i < listLength; i++){
+            finalString += listShortName[i].Regex;
+
+            let nextItemExists = i + 1 < listLength;
+            if(
+                // if the current item is optional
+                (listShortName[i].Options && listShortName[i].Options.Optional) ||
+                // if we are entering an end of line character
+                (nextItemExists && listShortName[i+1].Options && listShortName[i+1].Options.EndOfLine))
+                finalDelimiter = optionalDelimiter;
+            else 
+                finalDelimiter = delimiter;
+            finalString += finalDelimiter;
         }
+        //for(var control of $scope.currentRegexList){
+            
+        //}
 
         // removes the final \s+ from the end
-        return finalString.substr(0, finalString.length - delimiter.length);
+        return finalString.substr(0, finalString.length - finalDelimiter.length);
     }
 
     $scope.generateReplaceRegex = function() {
@@ -149,7 +190,10 @@ function regexHelperController($scope){
             for(var line of $scope.parsedLines) {
                 var lineOutput = "";
                 for(var match of line){
-                    lineOutput += match + delimiter;
+                    if(match)
+                        lineOutput += match + delimiter;
+                    else
+                        lineOutput += delimiter;
                 }
                 lineOutput = lineOutput.substr(0, lineOutput.length - delimiter.length) + newLine;
                 $scope.tabLinesOutput += lineOutput;
@@ -188,6 +232,11 @@ function regexHelperController($scope){
 
     $scope.handleControlClick = function(event, control){
         $scope.currentRegexList.push(control);
+        $scope.applyRegex();
+    }
+
+    $scope.handleRemoveLastItemClick = function(){
+        $scope.currentRegexList.pop();
         $scope.applyRegex();
     }
 };
